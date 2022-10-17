6e92b0953073d94af90a0f7b505aa84dcfb077ff
Lastllegacy <forrkko@gmail.com>
2022-10-17 22:48:29 +0700

    feat: done recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -12,14 +12,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
+// class DepthCalculator {
+//   answer = 1
+//   arrayOfDepths = []
+//   depth = 0
+//   calculateDepth(arr) {
+//     let isArray = 0
+//     for (let value of arr) {
+
+//       if(Array.isArray(value) && !isArray) {
+//         isArray = 1
+//         this.answer+=1;
+//         this.depth+=1;
+//         this.calculateDepth(value)
+//       } else if (Array.isArray(value)) {
+//         this.calculateDepth(value)
+//       }
+      
+//     }
+//     return this.answer
+//   }
+// }
+
 class DepthCalculator {
+  length = 1
   calculateDepth(arr) {
-    for (let value of arr) {
-      if(Array.isArray(value)) this.calculateDepth(arr)
+    let length = 1;
+    while(arr.some(value => Array.isArray(value) || value === [] ) ) {
+      arr = arr.flat();
+      this.length+=1;
     }
+    length = this.length;
+    this.length =1 ;
+    return length
   }
+  
 }
 
+const calculatePleaseSome = new DepthCalculator() 
+console.log(calculatePleaseSome.calculateDepth([1,[2,[3,[4,[123]]]],[5,[123],7,[123,[123,[123,[123]]]]],[6,[7]]]))
+console.log(calculatePleaseSome.calculateDepth([123]))
 module.exports = {
   DepthCalculator
 };
